5e7798e377f3e7d4acdc486a8f381c5275daf5dd
Dave Stanton <davestanton.us@gmail.com>
2017-12-21 18:01:29 -0500

    Use F instead of K units. I promise to squash commits after this.

--- open_weather_map.js
+++ open_weather_map.js
@@ -2,7 +2,7 @@ const WEATHER_API_KEY = '377b8e6d9708d0ed681e86d608ac4ffe';
 const API_STEM = 'https://api.openweathermap.org/data/2.5/weather?';
 
 function zipUrl(zip) {
-	return `${API_STEM}zip=${zip},us&appid=${WEATHER_API_KEY}`;
+	return `${API_STEM}zip=${zip},us&units=imperial&appid=${WEATHER_API_KEY}`;
 } 
         
 function fetchForecast(zip) { 
