389e0f06189679a0e5518a72c4a845bbe40614cb
Dave Stanton <davestanton.us@gmail.com>
2017-12-21 16:07:09 -0500

    iOS requires https

--- open_weather_map.js
+++ open_weather_map.js
@@ -1,5 +1,5 @@
 const WEATHER_API_KEY = '377b8e6d9708d0ed681e86d608ac4ffe';
-const API_STEM = 'http://api.openweathermap.org/data/2.5/weather?';
+const API_STEM = 'https://api.openweathermap.org/data/2.5/weather?';
 
 function zipUrl(zip) {
 	// return `${API_STEM}zip=${zip},us&appid=${WEATHER_API_KEY}`;
