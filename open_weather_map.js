8a4c0c3c8773f088120d8bd86a55f866cb7a076b
Dave Stanton <davestanton.us@gmail.com>
2017-12-21 17:17:24 -0500

    now fail is in App.js

--- open_weather_map.js
+++ open_weather_map.js
@@ -2,14 +2,13 @@ const WEATHER_API_KEY = '377b8e6d9708d0ed681e86d608ac4ffe';
 const API_STEM = 'https://api.openweathermap.org/data/2.5/weather?';
 
 function zipUrl(zip) {
-	// return `${API_STEM}zip=${zip},us&appid=${WEATHER_API_KEY}`;
-	return API_STEM + 'zip=' + zip + ',us&appid' + WEATHER_API_KEY;
+	return `${API_STEM}zip=${zip},us&appid=${WEATHER_API_KEY}`;
 } 
         
 function fetchForecast(zip) { 
 	return fetch(zipUrl(zip))
 		.then(response => response.json()) 
-		.then(responseJSON => { 
+		.then(responseJSON => {
 			return { 
 				main: responseJSON.weather[0].main,
 				description: responseJSON.weather[0].description,
